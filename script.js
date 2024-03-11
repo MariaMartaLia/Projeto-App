f89e87ec719d4f97ef9529469ccc9c2d27399ce5
Maria Marta (Lia) <160326282+MariaMartaLia@users.noreply.github.com>
2024-03-11 16:15:02 -0300

    cadpet

---
+++ script.js
@@ -0,0 +1,5 @@
+let nomepet=document.getElementById('nome')
+let racapet=document.getElementById('raca')
+let nometutor=document.getElementById('tutor')
+let tel =document.getElementById('tel')
+let cadastro=[]
